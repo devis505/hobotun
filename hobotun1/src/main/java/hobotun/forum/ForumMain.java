9cc89c852f324b729d84568b1811b266950a0fad
d.vasiliev <d.vasiliev@dvasiliev.abrr.local>
2016-02-05 14:00:09 +0300

    rename

---
+++ hobotun1/src/main/java/hobotun/forum/ForumMain.java
@@ -0,0 +1,35 @@
+package hobotun.forum;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import hobotun.db.DBUtil;
+import hobotun.db.forum.ForumDao;
+import hobotun.db.forum.table.ForumSectionTbl;
+
+@ManagedBean(name = "forumMain")
+@ViewScoped
+public class ForumMain implements Serializable {
+
+	private static final long serialVersionUID = 4207631485152406643L;
+	private List<ForumSectionTbl> forumSections;
+
+	public ForumMain() {
+
+		ForumDao forumDao = DBUtil.getInstance().getBean("forumDao", ForumDao.class);
+		forumSections = forumDao.getAllForumSection();
+
+	}
+
+	public List<ForumSectionTbl> getForumSections() {
+		return forumSections;
+	}
+
+	public void setForumSections(List<ForumSectionTbl> forumSections) {
+		this.forumSections = forumSections;
+	}
+
+}
