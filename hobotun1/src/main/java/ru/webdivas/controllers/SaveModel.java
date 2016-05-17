b46c54c3b77417dc0379d8f7104be0ea524886fa
Denis <Denis@192.168.1.70>
2016-05-17 22:33:15 +0300

    2

--- hobotun1/src/main/java/ru/webdivas/controllers/SaveModel.java
+++ hobotun1/src/main/java/ru/webdivas/controllers/SaveModel.java
@@ -3,28 +3,15 @@ package ru.webdivas.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import hobotun.db.model.tbl.ModelTbl;
 
 @Controller
 public class SaveModel {
 
-	@RequestMapping(value = "/uploadModel", method = RequestMethod.GET)
+	@RequestMapping(value = "/uploadModel", method = RequestMethod.POST)
 	public @ResponseBody String provideUploadInfo() {
 		return "Вы можете загружать файл с использованием того же URL.";
 	}
-	
-	@RequestMapping(value="/uploadModel", method=RequestMethod.POST)
-    public @ResponseBody String handleFileUpload(@RequestParam("model") ModelTbl model){
-        if (model == null) {
-        	
-        } else {
-            return "Не удалось загрузить файл.";
-        }
-        
-		return "Модель учано загружена, вы модете просмотреть её статус в разделе мои модели.";
-    }
+
 
 }
